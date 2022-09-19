c57ee9a3e18931d3caacf4da09344856ae53d319
Daniel Brathagen <dbrathagen@gmail.com>
2022-09-19 12:12:48 +0100

    Minor formatting

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,55 +1,53 @@
 import { readFile } from "fs/promises";
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import Layout from "../components/common/Layout";
-import { Post } from "../utils/types/Posts";
+import { Post } from "../utils/types/Post";
 import { marked } from "marked";
-import { Fragment } from "react";
 import parseHtml from "html-react-parser"
 import style from "../styles/Blog.module.scss"
 
 const Blog: NextPage = ({
   posts,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-    return (
-    <Layout description="Blog Routle">
-        <div className={style['post-container']}>
-            {posts
-                ?.sort((a: Post, b: Post) => b.id - a.id)
-                .map((post: Post) => {
-                return (
-                    <div className={style['post']}>
-                        <h2>{post.title}</h2>
-                        <h3>{post.date}</h3>
-                        <hr />
-                        <div className = {style['post-body']}>
-                            {parseHtml(post.body!)}
-                        </div>
-
-                    </div>
-                )
-            })}
-        </div>
-    </Layout>
-    )
+  return (
+  <Layout description="Blog Routle">
+    <div className={style['post-container']}>
+      {posts
+        ?.sort((a: Post, b: Post) => b.id - a.id)
+        .map((post: Post) => {
+          return (
+            <div className={style['post']}>
+              <h2>{post.title}</h2>
+              <h3>{post.date}</h3>
+              <hr />
+              <div className = {style['post-body']}>
+                {parseHtml(post.body!)}
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  </Layout>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    const posts = await readFile('public/blog/posts.json')
-    const parsedPosts: Post[] = JSON.parse(posts.toString());
+  const posts = await readFile('public/blog/posts.json')
+  const parsedPosts: Post[] = JSON.parse(posts.toString());
 
-    const completePosts = await Promise.all(parsedPosts.map(async (post) => {
-        const rawBody = await readFile(`public/blog/${post.bodyPath}`);
+  const completePosts = await Promise.all(parsedPosts.map(async (post) => {
+    const rawBody = await readFile(`public/blog/${post.bodyPath}`);
 
-        const body = await marked(rawBody.toString(), { async: true });
-        return { ...post, body};
-    }));
-    console.log(completePosts)
+    const body = await marked(rawBody.toString(), { async: true });
+    return { ...post, body};
+  }));
 
-    return {
-        props: {
-            posts: completePosts,
-        }
+  return {
+    props: {
+      posts: completePosts,
     }
+  }
 };
 
 export default Blog;
--- utils/types/Posts.ts
+++
@@ -1,7 +0,0 @@
-export interface Post {
-    id: number;
-    date: string;
-    title: string;
-    bodyPath: string;
-    body?: string;
-}

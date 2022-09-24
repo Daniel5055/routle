efc151a45b5e333d832a97ae07afe6b01143d927
Daniel Brathagen <dbrathagen@gmail.com>
2022-09-24 23:41:24 +0100

    More gameplay tests and made code more easily refactorable

--- cypress/e2e/singleplayer/gameplay.cy.js
+++ cypress/e2e/singleplayer/gameplay.cy.js
@@ -1,3 +1,9 @@
+// Helper functions
+const tooFar = (city) =>  `${city} is too far!`;
+const inRange = (city) => city;
+const alreadyIn = (city) =>  `Already in ${city}`;
+const noCity = (city) =>  `${city} ???`;
+
 describe('gameplay', () => {
 
   beforeEach(() => {
@@ -13,6 +19,9 @@ describe('gameplay', () => {
     ].forEach((city) => {
       cy.intercept(`https://secure.geonames.org/searchJSON?*${city}*`, { fixture: `${city.toLowerCase()}.json`}).as(city.toLowerCase());
     })
+
+    cy.intercept('https://secure.geonames.org/searchJSON?*Wrong*', { fixture: 'no-cities.json'}).as('wrong');
+
     cy.visit('/singleplayer/uk-ireland?c1=0&c2=59')
     cy.get('main input').as('input')
     cy.get('main p').as('tagline')
@@ -21,14 +30,14 @@ describe('gameplay', () => {
   it('can go to city in range', () => {
     const city = 'Oxford'
     cy.get('@input').type(`${city}{enter}`);
-    cy.get('@tagline').should('have.text', city)
+    cy.get('@tagline').should('have.text', inRange(city))
   })
 
   it('cannot go to city out of range', () => {
     const city = 'Sheffield'
 
     cy.get('@input').type(`${city}{enter}`);
-    cy.get('@tagline').should('have.text', `${city} is too far!`)
+    cy.get('@tagline').should('have.text', tooFar(city))
   })
 
   it('can go to multiple cities in range', () => {
@@ -36,10 +45,10 @@ describe('gameplay', () => {
     const city2 = 'Peterborough'
 
     cy.get('@input').type(`${city1}{enter}`);
-    cy.get('@tagline').should('have.text', city1)
+    cy.get('@tagline').should('have.text', inRange(city1))
 
     cy.get('@input').type(`${city2}{enter}`);
-    cy.get('@tagline').should('have.text', city2)
+    cy.get('@tagline').should('have.text', inRange(city2))
   })
 
   it('can go back to previous cities in range', () => {
@@ -52,20 +61,29 @@ describe('gameplay', () => {
 
     cities.forEach((city) => {
       cy.get('@input').type(`${city}{enter}`);
-      cy.get('@tagline').should('have.text', city)
+      cy.get('@tagline').should('have.text', inRange(city))
     })
   })
 
-  it('will go to next city of same name', () => {
+  it('will go to next city of same name in range', () => {
     const city1 = 'Oxford'
     const city2 = 'Wellington'
 
     cy.get('@input').type(`${city1}{enter}`);
-    cy.get('@tagline').should('have.text', city1)
+    cy.get('@tagline').should('have.text', inRange(city1))
     cy.get('@input').type(`${city2}{enter}`);
-    cy.get('@tagline').should('have.text', city2)
+    cy.get('@tagline').should('have.text', inRange(city2))
     cy.get('@input').type(`${city2}{enter}`);
-    cy.get('@tagline').should('have.text', city2)
+    cy.get('@tagline').should('have.text', inRange(city2))
+  })
+
+  it('will not go to next city of same name if not in range', () => {
+    const city = 'Oxford'
+
+    cy.get('@input').type(`${city}{enter}`);
+    cy.get('@tagline').should('have.text', inRange(city))
+    cy.get('@input').type(`${city}{enter}`);
+    cy.get('@tagline').should('have.text', tooFar(city))
   })
 
   it('will say if already in city', () => {
@@ -73,11 +91,11 @@ describe('gameplay', () => {
     const city2 = 'Peterborough'
 
     cy.get('@input').type(`${city1}{enter}`);
-    cy.get('@tagline').should('have.text', city1)
+    cy.get('@tagline').should('have.text', inRange(city1))
     cy.get('@input').type(`${city2}{enter}`);
-    cy.get('@tagline').should('have.text', city2)
+    cy.get('@tagline').should('have.text', inRange(city2))
     cy.get('@input').type(`${city2}{enter}`);
-    cy.get('@tagline').should('have.text', `Already in ${city2}`)
+    cy.get('@tagline').should('have.text', alreadyIn(city2))
   })
 
   it('will trigger win on going to end city in range', () => {
@@ -90,18 +108,32 @@ describe('gameplay', () => {
 
     cities.forEach((city) => {
       cy.get('@input').type(`${city}{enter}`);
-      cy.get('@tagline').should('have.text', city)
+      cy.get('@tagline').should('have.text', inRange(city))
     })
 
     cy.get('@input').type(`${endCity}{enter}`);
     cy.get('@tagline').should('have.text', `You win!`)
   })
 
-  it('Cannot go to end city out of range', () => {
+  it('cannot go to end city out of range', () => {
     const endCity = 'York'
 
     cy.get('@input').type(`${endCity}{enter}`);
-    cy.get('@tagline').should('have.text', `${endCity} is too far!`)
+    cy.get('@tagline').should('have.text',tooFar(endCity))
+  })
+
+  it('will go to nearest city of same name', () => {
+    const city1 = 'Oxford'
+    const city2 = 'Cambridge'
+    const city3 = 'Peterborough'
+
+    // Goes to a different cambridge so peterborough is too far now
+    cy.get('@input').type(`${city1}{enter}`);
+    cy.get('@tagline').should('have.text', inRange(city1));
+    cy.get('@input').type(`${city2}{enter}`);
+    cy.get('@tagline').should('have.text', inRange(city2));
+    cy.get('@input').type(`${city3}{enter}`);
+    cy.get('@tagline').should('have.text', tooFar(city3));
   })
 
   it('will always go to end city if in range', () => {
@@ -110,8 +142,14 @@ describe('gameplay', () => {
     const city2 = 'Cambridge'
 
     cy.get('@input').type(`${city1}{enter}`);
-    cy.get('@tagline').should('have.text', city1)
+    cy.get('@tagline').should('have.text', inRange(city1));
     cy.get('@input').type(`${city2}{enter}`);
     cy.get('@tagline').should('have.text', 'You win!')
+  })
+
+  it('will not go to city not found', () => {
+    const city = 'Wrong'
+    cy.get('@input').type(`${city}{enter}`);
+    cy.get('@tagline').should('have.text', noCity(city));
   })
 })

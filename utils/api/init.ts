bb23a457a2024873bbdc4006385d8f39f6824a17
Daniel Brathagen <dbrathagen@gmail.com>
2022-09-27 00:17:01 +0100

    Updated local env names to be google specific

--- utils/api/init.ts
+++ utils/api/init.ts
@@ -11,16 +11,18 @@ const app = (() => {
     // Init and configure app
     return admin.initializeApp({
       credential: cert({
-        type: "service_account",
-        project_id: "routle-db",
-        private_key_id: process.env.private_key_id,
-        private_key: process.env.private_key,
-        client_email: process.env.client_email,
-        client_id: process.env.client_id,
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://oauth2.googleapis.com/token",
-        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ll0wy%40routle-db.iam.gserviceaccount.com"
+        type: 'service_account',
+        project_id: 'routle-db',
+        private_key_id: process.env.google_private_key_id,
+        private_key: process.env.google_private_key,
+        client_email: process.env.google_client_email,
+        client_id: process.env.google_client_id,
+        auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+        token_uri: 'https://oauth2.googleapis.com/token',
+        auth_provider_x509_cert_url:
+          'https://www.googleapis.com/oauth2/v1/certs',
+        client_x509_cert_url:
+          'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ll0wy%40routle-db.iam.gserviceaccount.com',
       } as ServiceAccount),
       databaseURL: 'https://routle-db.firebaseio.com',
     });

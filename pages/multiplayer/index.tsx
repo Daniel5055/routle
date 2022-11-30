849f8a33748be7c3eb78574b26257e79e312bee9
Daniel Brathagen <dbrathagen@gmail.com>
2022-11-30 23:27:16 +0100

    Added basic lobby creation for multiplayer Successfuly communication with websocket server

--- pages/multiplayer/index.tsx
+++ pages/multiplayer/index.tsx
@@ -1,13 +1,28 @@
 import { NextPage } from 'next';
 import Layout from '../../components/common/Layout';
 import { useMobile } from '../../components/hooks/MobileHook';
+import styles from '../../styles/Multiplayer.module.scss';
+import { useRouter } from 'next/router';
 
 const Multiplayer: NextPage = () => {
   const isMobile = useMobile();
+  const router = useRouter();
 
+  const url = 'http://localhost:23177';
+
+  async function hostGame() {
+    const response = await fetch(`${url}/host-game`, {
+      method: 'POST',
+    }).then((res) => res.json());
+    const gameId = response.id;
+    router.push(`/multiplayer/${gameId}`);
+  }
   return (
     <Layout isMobile={isMobile}>
-      <p>Work in progress</p>
+      <p>Join a game with a url or host one here:</p>
+      <button className={styles.option} onClick={hostGame}>
+        Host Game
+      </button>
     </Layout>
   );
 };

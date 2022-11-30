f73971ebccf2ac5ed1ba1b3989720b8976b9fc69
Daniel Brathagen <dbrathagen@gmail.com>
2022-12-01 00:57:20 +0100

    Added basic dynamic player listing in multiplayer

--- pages/multiplayer/[game].tsx
+++ pages/multiplayer/[game].tsx
@@ -1,9 +1,12 @@
+import { raw } from 'body-parser';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import Layout from '../../components/common/Layout';
 import { useMobile } from '../../components/hooks/MobileHook';
+import styles from '../../styles/Multiplayer.module.scss';
+import { Player } from '../../utils/types/multiplayer/Player';
 
 const Game: NextPage = () => {
   const isMobile = useMobile();
@@ -13,19 +16,54 @@ const Game: NextPage = () => {
 
   const { game: gameId } = router.query;
 
+  const [isValid, setIsValid] = useState(true);
   const [isLeader, setIsLeader] = useState(false);
 
-  const server = io(url);
+  const [players, setPlayers] = useState<Player[]>([]);
 
-  server.emit('join-game', gameId);
+  useEffect(() => {
+    const server = io(url);
+    gameId && server.emit('join-game', gameId);
+    server.on('new-leader', (id) => {
+      setIsLeader(server.id === id);
+    });
 
-  server.on('new-leader', (id) => {
-    setIsLeader(server.id === id);
-  });
+    server.on('players', (msg) => {
+      const parsedPlayers = (JSON.parse(msg) as Player[]).map((player, i) => {
+        player.you = player.id === server.id;
+        player.leader = i === 0;
+
+        return player;
+      });
+      setPlayers(parsedPlayers);
+    });
+
+    server.on('not-found', () => {
+      setIsValid(false);
+    });
+  }, [gameId]);
 
   return (
     <Layout isMobile={isMobile}>
-      <p>Here {gameId}!</p>
+      {isValid ? (
+        <div id={styles['start-container']}>
+          <div id={styles['players']} className={styles['container']}>
+            <h2>Players</h2>
+            <div id={styles['player-list']}>
+              {players.map((player) => (
+                <div key={player.id}>
+                  <p>{player.you ? <b>{player.name}</b> : player.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div id={styles['settings']} className={styles['container']}>
+            <h2>Game Settings</h2>
+          </div>
+        </div>
+      ) : (
+        <h2>Game not found</h2>
+      )}
     </Layout>
   );
 };

3417a1ce36c79bbc1d22e734dc0f79da8b81ad67
Daniel Brathagen <dbrathagen@gmail.com>
2022-12-24 17:41:59 +0100

    Added timer

---
+++ components/multiplayer/Timer.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+export const Timer = (props: { state: 'idle' | 'start' | 'countdown' }) => {
+  const [time, setTime] = useState(0);
+  const [countdownTime, setCountdownTime] = useState(30 + 1);
+
+  useEffect(() => {
+    if (props.state !== 'idle') {
+      const id = setInterval(() => {
+        setTime((t) => t + 1);
+      }, 1000);
+
+      return () => clearInterval(id);
+    }
+  }, [props.state]);
+
+  useEffect(() => {
+    if (props.state === 'countdown') {
+      setCountdownTime((t) => (t <= 0 ? 0 : t - 1));
+    }
+  }, [props.state, time]);
+
+  return (
+    <>
+      <h2>
+        {Math.floor(time / 60)}:{(time % 60).toString().padStart(2, '0')}
+      </h2>
+      <p>
+        {props.state === 'idle'
+          ? 'Starting soon!'
+          : props.state === 'countdown'
+          ? `You have ${countdownTime} seconds left`
+          : props.state === 'start'
+          ? 'Get there first!'
+          : '???'}
+      </p>
+    </>
+  );
+};
